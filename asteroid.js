0652b99290e98203ba31077736f69355e3710273
west-mec-ryan <ryan.morales@west-mec.org>
2023-05-08 15:49:10 -0700

    added asteroid

---
+++ asteroid.js
@@ -0,0 +1,43 @@
+// import { createShip } from "./ship";
+
+export class Asteroid {
+  constructor(canvas, size) {
+    this.canvas = canvas;
+    this.size = size;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.rotation = Math.random() * Math.PI * 2;
+    this.velocity = {
+      x: Math.random() * 2 - 1, // Random x velocity between -1 and 1
+      y: Math.random() * 2 - 1, // Random y velocity between -1 and 1
+    };
+  }
+
+  draw(ctx) {
+    // Draw the asteroid as a simple circle
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+    ctx.fillStyle = "#22aacc";
+    ctx.fill();
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.stroke();
+  }
+
+  update() {
+    // Update the asteroid position based on its velocity
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    // Bounce the asteroid when it hits the edge of the canvas
+    if (this.x < 0 || this.x > this.canvas.width) {
+      this.velocity.x = -this.velocity.x;
+    }
+    if (this.y < 0 || this.y > this.canvas.height) {
+      this.velocity.y = -this.velocity.y;
+    }
+  }
+}
+
+export function createAsteroid(canvas, size) {
+  return new Asteroid(canvas, size);
+}
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import { createShip } from "./ship.js";
 import { animateLoop } from "./animationLoop.js";
 import { createBullet } from "./bullet.js";
+import { createAsteroid } from "./asteroid.js";
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let size;
 const ship = createShip(canvas, Math.min(canvas.width, canvas.height) * 0.1);
+const asteroid = createAsteroid(
+  canvas,
+  Math.min(canvas.width, canvas.height) * 0.1
+);
+
 const bullets = [];
 // add a cooldown mechanism to slow down bullets
 const bulletCooldown = 300;
@@ -31,6 +37,7 @@ function resizeCanvas() {
   // add ship x and y to resize
   ship.x = canvas.width / 2;
   ship.y = canvas.height / 2;
+  asteroid.size = size * 1.5;
   // drawTriangle(canvas, ctx, size);
 }
 
@@ -96,7 +103,8 @@ animateLoop(
     }
     ship.draw(ctx);
     ship.update();
-
+    asteroid.draw(ctx);
+    asteroid.update();
     bullets.forEach((bullet) => {
       bullet.draw(ctx);
       bullet.update();
